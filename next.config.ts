b99ce1e3b926c3bed956852a0ccf83c1ee034db7
Tony Ko <tony@tko.dev>
2026-03-15 10:06:24 -0400

    add ig redirect

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/ig',
+        destination: 'https://www.instagram.com/tkodev',
+        permanent: true,
+        basePath: false
+      }
+    ]
+  }
 }
 
 export default nextConfig
